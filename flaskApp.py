5287e2c816986850101a2ad03d03589f0d605bb0
mangalshashank <mangalshashank1@gmail.com>
2023-09-30 22:55:58 +0530

    Added Location Verification

--- flaskApp.py
+++ flaskApp.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+import geolocation
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/invalid_location')
+def invalid_location():
+    return render_template('invalid_location.html')
+
+@app.route('/agent_verification')
+def agent_verification():
+    return render_template('agent_verification.html')
+
+@app.route('/location_verification', methods=['POST'])
+def location_verification():
+    validLocation = geolocation.verifyLocation()  
+    if validLocation:
+        return redirect(url_for('agent_verification'))
+    else:
+        return redirect(url_for('invalid_location'))
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- geolocation.py
+++ geolocation.py
@@ -4,7 +4,6 @@ from geopy.distance import geodesic
 def get_current_location_coordinates():
     # Use the 'ipinfo' provider to get the user's current location based on IP address.
     g = geocoder.ip('me')
-
     if g.latlng:
         latitude, longitude = g.latlng
         return latitude, longitude
@@ -15,7 +14,7 @@ authorized_locations = []
 
 def verifyLocation():
     coordinates = get_current_location_coordinates()
-    authorized_locations.append(coordinates)
+    #authorized_locations.append(coordinates)
     for auth_location in authorized_locations:
         distance = geodesic(coordinates, auth_location).meters
         if distance < 100:
